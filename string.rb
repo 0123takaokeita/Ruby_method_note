86c4f877c589cc0c65559a1b469b779f07fe443d
takefumi <65272471+0123takaokeita@users.noreply.github.com>
2021-05-09 14:28:04 +0900

    add string method

--- string.rb
+++ string.rb
@@ -1,39 +1,64 @@
 # 文字列の取り扱い
+str = "abcdefghijk"  
 
-str = "abcdefghijk"
-
-# 文字列を数字で返す
+# 文字列を数えて数値で返す
 p str.length
+# 11
 
 # indexが振られているので指定できる
 p str[0]
+# "a"
 
 # index範囲指定
-p str[0..4]
+p str[0..3]
+# "abcd"
 
 # indexで負の数字を扱うときは文字列の左から順位書く注意が必要。
-p str[-5..-1]
+p str[-4..-1]
+# "hijk"
 
 # 置換する。AをBに置換する。
 p str.gsub("b","a")
+# "aacdefghijk"
 
 # 指定した文字を切り出す。
 p str.slice(1)
+# "b"
+
+# 指定の文字が含まれるか判定する。
+p str.include?("a")
+# true
 
 # 小文字を大文字に変換する
 p STR = str.upcase
+# "ABCDEFGHIJK"
 
 # 大文字を小文字に
 p restr = STR.downcase
+# "abcdefghijk"
 
-# end_with?（指定の文字列で終わっているか調べる）
-p str.end_with?("a","jk")
+# 文字列の終わりを判定して真偽値で返す。複数選択可能。
+p str.end_with?("a")
+# true
+
+# 文字列の始まりを判定して真偽値で返す。複数選択可能。
+p str.start_with?("a")
+# true
 
 # 特定の文字を数える
 p str.count("a")
+# 1
 
 # 
 p str.scan("a")
 
-# 正規表現で検索する
-p str =~ /[z]/
+# 正規表現で判定する。０かnilがかえる。
+p str =~ /[a]/
+# true
+
+# 指定の文字列に書き換える。（破壊する）
+p STR.replace("aaa")
+#  "aaa"
+
+#　指定の場所に差し込む
+p str.insert(1,"123")
