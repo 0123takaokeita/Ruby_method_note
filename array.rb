8de4148f2399f2714b19077bed9c3a731f45fc33
takefumi <65272471+0123takaokeita@users.noreply.github.com>
2021-06-06 14:11:38 +0900

    並び替え色々

--- array.rb
+++ array.rb
@@ -3,6 +3,9 @@ array = (1..7).to_a
 # 配列の合計
 p array.sum
 
+# 配列を数える
+p array.length
+
 # 配列の結合（結合するとstringになる）
 p array.join
 p array.join(',')
@@ -26,8 +29,15 @@ p array.reverse
 
 # 重複の削除
 p array << 1
-p array.uniq
+p array.uniq!
 
 # 同じ処理を繰り返す
 p array.map(&:to_s)
+
+# 引数を数えて返す
+p array.count(1)
+
+# 昇順に並び替える。
+NewArray = array.reverse
+p array.sort
 
