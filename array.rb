3ae28a6ea9ee887b773af6a8612075abfc56ac06
takefumi <65272471+0123takaokeita@users.noreply.github.com>
2021-06-06 12:42:16 +0900

    メソッド追加

---
+++ array.rb
@@ -0,0 +1,24 @@
+array = (1..7).to_a
+
+# 配列の合計
+p array.sum
+
+# 配列の結合（結合するとstringになる）
+p array.join
+p array.join(',')
+
+# 配列に追加
+p array.push(8)
+
+# 末尾を削除
+array.pop
+p array
+
+# 先頭に追加
+p array.unshift(0)
+
+# 先頭を削除
+array.shift
+p array
+
+
